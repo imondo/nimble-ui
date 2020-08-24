b0e3cc8193f3df22c8e7a3140997b53c983954d9
Mondo <wuyw@sqqmall.com>
2020-08-24 18:33:22 +0800

    build: 发布文档配置

--- .umirc.ts
+++ .umirc.ts
@@ -6,5 +6,14 @@ export default defineConfig({
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
   mode: 'site',
-  // more config: https://d.umijs.org/config
+  base: '/qku-ui',
+  publicPath: '/qku-ui/',
+  exportStatic: {}, // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
+  theme: {
+    '@c-primary': '#0876e4',
+  },
+  locales: [
+    ['zh-CN', '中文'],
+    ['en-US', 'English'],
+  ],
 });
