5a338716eb47db434ac21046492957ed644bcfac
Mondo <wuyw@sqqmall.com>
2020-08-27 13:02:42 +0800

    feat: 抽屉组件添加动画效果

--- src/Drawer/index.tsx
+++ src/Drawer/index.tsx
@@ -1,20 +1,26 @@
 import React, { useEffect, useState } from 'react';
+import { createPortal } from 'react-dom';
 import './index.less';
 
+const _container = document.createElement('div');
+_container.classList.add('drawer__wrapper');
+
 export default (props: any) => {
-  const [defaultHeight, setDefaultHeight] = useState(props.height || '30%');
+  const [defaultHeight, setDefaultHeight] = useState(props.height);
 
   useEffect(() => {
+    document.body.appendChild(_container);
     if (props.open) {
       document.body.classList.add('overflow-hidden');
     } else {
       document.body.classList.remove('overflow-hidden');
+      document.body.removeChild(_container);
     }
 
     if (['right', 'left'].includes(props.position)) {
-      setDefaultHeight('100%');
+      // setDefaultHeight('100%');
     } else {
-      setDefaultHeight('30%');
+      // setDefaultHeight('30%');
     }
   });
 
@@ -23,19 +29,20 @@ export default (props: any) => {
     document.body.classList.remove('overflow-hidden');
   };
 
-  return (
-    <div className={`drawer ${props.open ? 'drawer-open' : ''}`}>
+  return props.open && createPortal(
+    <div className={`drawer drawer-open`}>
       <div
         className={`drawer-body ${
           props.position
             ? 'drawer-body--' + props.position
             : 'drawer-body--default'
-        }`}
+        } ${props.open ? 'drawer-body-open--' + props.position : ''}`}
         style={{ height: `${defaultHeight}` }}
       >
         {props.children}
       </div>
       <div className="drawer-overlay" onClick={e => onClickBody(props)}></div>
-    </div>
+    </div>,
+    _container,
   );
 };
